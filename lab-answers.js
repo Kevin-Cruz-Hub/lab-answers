5eae9cb3bb9f9089e6dff5fa9898d09b41b7ef19
Kevin <kevincruz042@gmail.com>
2023-06-29 17:41:12 -0400

    Get Even answered

--- lab-answers.js
+++ lab-answers.js
@@ -1,3 +1,11 @@
+// Easy Going answered
 for(let i = 0; i <= 20; i++){
     console.log(i)
+}
+
+// Get Even answered
+for(let i = 0; i <= 200; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
 }

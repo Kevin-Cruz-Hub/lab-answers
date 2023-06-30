b75c68c8b46a3f552127626f078d59bd96d9eb48
Kevin <kevincruz042@gmail.com>
2023-06-29 20:38:38 -0400

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -1,11 +1,24 @@
-// Easy Going answered
-for(let i = 0; i <= 20; i++){
-    console.log(i)
-}
+// // Easy Going answered
+// for(let i = 0; i <= 20; i++){
+//     console.log(i)
+// }
 
-// Get Even answered
-for(let i = 0; i <= 200; i++){
-    if(i % 2 === 0){
-        console.log(i)
-    }
-}
+// // Get Even answered
+// for(let i = 0; i <= 200; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// // Fizz Buzz answered
+// for(let i = 0; i <= 100; i++){
+//   if(i % 3 === 0 && i % 5 === 0){
+//     console.log('FizzBuzz');
+//   }else if(i % 3 === 0){
+//     console.log('Fizz');
+//   }else if(i % 5 === 0){
+//     console.log('Buzz');
+//   }else{
+//     console.log(i);
+//   }
+// }
